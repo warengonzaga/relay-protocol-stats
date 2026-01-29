7300156bcc7158679e1675c125e9ff35a76dd760
Waren Gonzaga <opensource@warengonzaga.com>
2026-01-29 18:59:51 +0800

    ⚙️ setup: define app version from package.json in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import packageJson from './package.json'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,5 +11,8 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  define: {
+    __APP_VERSION__: JSON.stringify(packageJson.version),
   },
 })
